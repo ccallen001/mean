1215d083ad4d86dfa47095d331f96f63f28caf6d
Chase Allen <ccallen001@gmail.com>
2023-01-08 18:32:43 -0500

    create a mean stack app

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="container-md">
+      <router-outlet></router-outlet>
+    </div>
+  `,
+  styles: []
+})
+
+export class AppComponent {}
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { EmployeeFormComponent } from './employee-form/employee-form.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+
+@NgModule({
+  declarations: [AppComponent, EmployeesListComponent, EmployeeFormComponent, AddEmployeeComponent, EditEmployeeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
